75926a8bd880dac5b4d15510bc62fd389a1e9976
71Arid <no232el@gmail.com>
2024-04-03 16:54:43 +0300

    basic cache class used super

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""
+You must use self.cache_data - dictionary from the
+parent class BaseCaching
+This caching system doesn’t have limit
+"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """
+    inherits from BaseCaching and implements
+    put and get methods
+    """
+    def __init__(self):
+        """
+        initializes class by calling constructor of super class
+        """
+        super().__init__()
+
+    def put(self, key, item):
+        """
+        updates cache_data whic is declared in
+        super class
+        """
+        if key is None and item is None:
+            return
+        return self.cache_data.update({key: item})
+
+    def get(self, key):
+        """
+        access value stored by a particular key in super class's
+        cache_data
+        """
+        if key is None:
+            return None
+        return self.cache_data.get(key)
