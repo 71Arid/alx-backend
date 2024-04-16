6d14968c0428d8ceb80c2f27a5c606e962f469d9
71Arid <no232el@gmail.com>
2024-04-16 21:58:58 +0300

    committing the first task 0-app.py and index.html

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""0-app.py
+ Create a single / route and an index.html template that simply
+ outputs “Welcome to Holberton” as page title (<title>)
+ and “Hello world” as header (<h1>).
+"""
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+app.run()
