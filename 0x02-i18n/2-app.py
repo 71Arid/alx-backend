30feb624a0184fef7f50bf41dd7b08919f18ca19
71Arid <no232el@gmail.com>
2024-04-17 12:23:13 +0300

    added .html files

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Creates a get_locale function with the babel.localeselector decorator.
+"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+babel = Babel(app)
+@babel.localeselector
+def get_locale():
+    """
+    gets the best match from the supported
+    languages
+    """
+    return request.accept_languages.best_match(["en", "fr"])
+
+
+@app.route('/')
+def get_index():
+    """get the home index for the babel app
+    """
+    return render_template("2-index.html")
+
+
+if __name__ == "__main__":
+    app.run()
