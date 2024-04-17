bfa0f4cd415a065231b2219e7511e69580815302
71Arid <no232el@gmail.com>
2024-04-17 12:29:19 +0300

    removed unecessary comments in o0 app

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,16 +1,14 @@
 #!/usr/bin/env python3
 """0-app.py
  Create a single / route and an index.html template that simply
- outputs “Welcome to Holberton” as page title (<title>)
- and “Hello world” as header (<h1>).
+ outputs Welcome to Holberton as page title (<title>)
+ and Hello world as header (<h1>).
 """
 from flask import Flask, render_template
 
-# Create a Flask application instance
 app = Flask(__name__)
 
 
-# Define a route for the root URL ('/')
 @app.route('/')
 def get_index():
     """Renders the HTML page containing the contents
@@ -18,6 +16,5 @@ def get_index():
     return render_template('0-index.html')
 
 
-# Run the Flask application
 if __name__ == "__main__":
     app.run()
