3b4add1dcc2b016a289c73d71252a0c66c93eea0
71Arid <no232el@gmail.com>
2024-04-18 16:15:06 +0300

    4-app.py

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""
+In this task, you will implement a way
+to force a particular locale by passing
+the locale=fr parameter to your apps URLs.
+"""
+from flask import Flask, render_template
+from flask_babel import Babel, gettext, request
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """gets locale from the headers"""
+    lang = request.args.get("locale")
+    print(lang)
+    if lang:
+        return lang
+    return request.accept_languages.best_match(["en", "fr"])
+    
+
+@app.route('/')
+def get_index():
+    """initializes the paremeters for index.html
+    """
+    home_title = gettext("home_title")
+    home_header = gettext("home_header")
+    return render_template(
+        "4-index.html", home_title=home_title, home_header=home_header
+    )
+
+
+if __name__ == "__main__":
+    app.run()
